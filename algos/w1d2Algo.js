64f9ecbd0bf941c6066b58a60d9f6606ff342d6e
Yurochkina <olga.yurochkina@gmail.com>
2022-06-22 11:28:39 -0500

    Added algo file

---
+++ algos/w1d2Algo.js
@@ -0,0 +1,35 @@
+
+// data types
+
+
+var name = "Olga";
+var age = 2;
+var isTeachingToday = true;
+
+console.log(name);
+
+
+// operators
+
+// division 
+var halfAge = age/2;
+
+// multiplication
+var twiceMyAge = age*2;
+
+
+
+
+// data structures
+// array
+
+var artist = ["Drake", "Weezy", "Swift"]
+// indexes -->   0        1        2
+
+console.log(artist[0]);
+
+// loops
+
+for (var i = 0; i < artist.length; i++){
+    console.log(artist[i]);
+}
